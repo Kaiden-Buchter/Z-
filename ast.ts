8e49a0ad59b6ac7375416683b7774cb477a402ae
Kaiden Buchter <150194619+Kaiden-Buchter@users.noreply.github.com>
2023-12-06 16:48:10 +0000

    Update AST interface definitions

--- ast.ts
+++ ast.ts
@@ -1,4 +1,5 @@
 // Define the possible types of nodes in the AST (Abstract Syntax Tree)
+// Each type corresponds to a different kind of operation or value in the program
 export type NodeTypes =
   | "Program" // Represents the entire program
   | "NumericLiteral" // Represents a numeric literal (e.g., 1, 2, 3)
@@ -13,8 +14,8 @@ export interface Stmt {
 
 // A Program node represents the entire program. It contains a body which is a list of statements
 export interface Program extends Stmt {
-    kind: "Program",
-    body: Stmt[];
+    kind: "Program", // The type of the node
+    body: Stmt[]; // The list of statements in the program
 }
 
 // Expressions are operations that produce a value
@@ -23,7 +24,7 @@ export interface Expr extends Stmt {}
 
 // A BinaryExpr node represents a binary expression. It contains a left and right expression and an operator
 export interface BinaryExpr extends Expr {
-    kind: "BinaryExpr",
+    kind: "BinaryExpr", // The type of the node
     left: Expr, // The left-hand side of the expression
     right: Expr, // The right-hand side of the expression
     operator: string // The operator (e.g., +, -, *, /)
@@ -31,12 +32,12 @@ export interface BinaryExpr extends Expr {
 
 // An Identifier node represents an identifier (e.g., variable name)
 export interface Identifier extends Expr {
-    kind: "Identifier",
+    kind: "Identifier", // The type of the node
     symbol: string // The name of the identifier
 }
 
 // A NumericLiteral node represents a numeric literal
 export interface NumericLiteral extends Expr {
-    kind: "NumericLiteral",
+    kind: "NumericLiteral", // The type of the node
     value: number // The value of the numeric literal
 }
