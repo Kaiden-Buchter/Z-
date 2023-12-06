046bfc4c77ede52c77c4a0dcc9fc603e0cfcc56a
Kaiden Buchter <150194619+Kaiden-Buchter@users.noreply.github.com>
2023-12-06 21:16:19 +0000

    Refactor REPL loop and added environment variables

--- frontend/ast.ts
+++ frontend/ast.ts
@@ -3,25 +3,24 @@
 // ---     Defines the structure of our Languages AST      ---
 // -----------------------------------------------------------
 
-
 // Define the possible types of nodes in the AST (Abstract Syntax Tree)
 // Each type corresponds to a different kind of operation or value in the program
 export type NodeTypes =
   | "Program" // Represents the entire program
   | "NumericLiteral" // Represents a numeric literal (e.g., 1, 2, 3)
   | "Identifier" // Represents an identifier (e.g., variable name)
-  | "BinaryExpr" // Represents a binary expression (e.g., a + b)
+  | "BinaryExpr"; // Represents a binary expression (e.g., a + b)
 
 // Statements are operations that do something but do not produce a value
 // For example, a variable declaration (let x = 45) is a statement
 export interface Stmt {
-    kind: NodeTypes // The type of the node
+  kind: NodeTypes; // The type of the node
 }
 
 // A Program node represents the entire program. It contains a body which is a list of statements
 export interface Program extends Stmt {
-    kind: "Program", // The type of the node
-    body: Stmt[]; // The list of statements in the program
+  kind: "Program"; // The type of the node
+  body: Stmt[]; // The list of statements in the program
 }
 
 // Expressions are operations that produce a value
@@ -30,20 +29,20 @@ export interface Expr extends Stmt {}
 
 // A BinaryExpr node represents a binary expression. It contains a left and right expression and an operator
 export interface BinaryExpr extends Expr {
-    kind: "BinaryExpr", // The type of the node
-    left: Expr, // The left-hand side of the expression
-    right: Expr, // The right-hand side of the expression
-    operator: string // The operator (e.g., +, -, *, /)
+  kind: "BinaryExpr"; // The type of the node
+  left: Expr; // The left-hand side of the expression
+  right: Expr; // The right-hand side of the expression
+  operator: string; // The operator (e.g., +, -, *, /)
 }
 
 // An Identifier node represents an identifier (e.g., variable name)
 export interface Identifier extends Expr {
-    kind: "Identifier", // The type of the node
-    symbol: string // The name of the identifier
+  kind: "Identifier"; // The type of the node
+  symbol: string; // The name of the identifier
 }
 
 // A NumericLiteral node represents a numeric literal
 export interface NumericLiteral extends Expr {
-    kind: "NumericLiteral", // The type of the node
-    value: number // The value of the numeric literal
+  kind: "NumericLiteral"; // The type of the node
+  value: number; // The value of the numeric literal
 }
--- frontend/parser.ts
+++ frontend/parser.ts
@@ -1,77 +1,128 @@
-import { Stmt, Program, Expr, BinaryExpr, NumericLiteral, Identifier } from "./ast.ts";
+// deno-lint-ignore-file no-explicit-any
+import {
+  Stmt,
+  Program,
+  Expr,
+  BinaryExpr,
+  NumericLiteral,
+  Identifier,
+} from "./ast.ts";
 import { tokenize, Token, TokenType } from "./lexer.ts";
 
 export default class Parser {
-    private tokens: Token[] = [];
+  private tokens: Token[] = [];
 
-    private not_eof(): boolean {
-        return this.tokens[0].type != TokenType.EOF;
+  private not_eof(): boolean {
+    return this.tokens[0].type != TokenType.EOF;
+  }
+
+  private at() {
+    return this.tokens[0] as Token;
+  }
+
+  private eat() {
+    const prev = this.tokens.shift() as Token;
+    return prev;
+  }
+
+  private expect(type: TokenType, err: any) {
+    const prev = this.tokens.shift() as Token;
+    if (!prev || prev.type != type) {
+      console.log("Parser Error:\n", err, prev, "- Expecting: ", type);
+      Deno.exit(1);
     }
 
-    private at () {
-        return this.tokens[0] as Token; 
+    return prev;
+  }
+
+  public produceAST(sourceCode: string): Program {
+    this.tokens = tokenize(sourceCode);
+    const program: Program = {
+      kind: "Program",
+      body: [],
+    };
+
+    // Parse until EOF
+    while (this.not_eof()) {
+      program.body.push(this.parse_stmt());
     }
 
-    private eat () {
-        const prev = this.tokens.shift() as Token;
-        return prev;
+    return program;
+  }
+
+  private parse_stmt(): Stmt {
+    // skip to parse_expr
+    return this.parse_expr();
+  }
+
+  private parse_expr(): Expr {
+    return this.parse_additive_expr();
+  }
+
+  private parse_additive_expr(): Expr {
+    let left = this.parse_multiplicitave_expr();
+
+    while (this.at().value == "+" || this.at().value == "-") {
+      const operator = this.eat().value;
+      const right = this.parse_multiplicitave_expr();
+      left = {
+        kind: "BinaryExpr",
+        left,
+        right,
+        operator,
+      } as BinaryExpr;
     }
 
-    public produceAST (sourceCode: string): Program {
-        this.tokens = tokenize(sourceCode);
-        const program: Program = {
-            kind: "Program",
-            body: []
-        };
+    return left;
+  }
 
-        // Parse until EOF
-        while (this.not_eof()) {
-            program.body.push(this.parse_stmt());
-        }
+  private parse_multiplicitave_expr(): Expr {
+    let left = this.parse_primary_expr();
 
-        return program;
+    while (
+      this.at().value == "/" ||
+      this.at().value == "*" ||
+      this.at().value == "%"
+    ) {
+      const operator = this.eat().value;
+      const right = this.parse_primary_expr();
+      left = { kind: "BinaryExpr", left, right, operator } as BinaryExpr;
     }
 
-    private parse_stmt(): Stmt {
-        // skip to parse_expr
-        return this.parse_expr();
+    return left;
+  }
+
+  // Orders Of Prescidence
+  // AdditiveExpr
+  // MultiplicativeExpr
+  // PrimaryExpr
+
+  private parse_primary_expr(): Expr {
+    const tk = this.at().type;
+
+    switch (tk) {
+      case TokenType.Identifier:
+        return { kind: "Identifier", symbol: this.eat().value } as Identifier;
+
+      case TokenType.Number:
+        return {
+          kind: "NumericLiteral",
+          value: parseFloat(this.eat().value),
+        } as NumericLiteral;
+
+      case TokenType.OpenParen: {
+        this.eat(); // eat the open paren
+        const value = this.parse_expr();
+        this.expect(
+          TokenType.CloseParen,
+          "Unexpected token found inside parenthesised expression. Expected closing parenthesis."
+        ); // eat the close paren
+        return value;
+      }
+
+      default:
+        console.log("Unreconized token found during parsing! ", this.at());
+        Deno.exit(1);
     }
-
-    private parse_expr(): Expr {
-        return this.parse_additive_expr();
-    }
-
-    // (10 + 5) - 5
-    private parse_additive_expr(): Expr {
-        let left = this.parse_primary_expr();
-
-        while (this.at().value == "+" || this.at().value == "-") {
-            const operator = this.eat().value;
-            const right = this.parse_primary_expr();
-            left = { kind: "BinaryExpr", left, right, operator } as BinaryExpr;
-        }
-
-        return left;
-    }
-
-    // Orders Of Prescidence
-    // AdditiveExpr
-    // MultiplicativeExpr
-    // PrimaryExpr
-    
-    private parse_primary_expr(): Expr {
-        const tk = this.at().type;
-
-        switch (tk) {
-            case TokenType.Identifier:
-                return { kind: "Identifier", symbol: this.eat().value } as Identifier;
-            case TokenType.Number:
-                return { kind: "NumericLiteral", value: parseFloat(this.eat().value) } as NumericLiteral;
-    
-        default:
-            console.log("Unreconized token found during parsing!: ", this.at());
-            Deno.exit(1);
-        }
-
-    }
+  }
 }
