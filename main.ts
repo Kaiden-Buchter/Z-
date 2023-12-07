76247cb77fab74de389c4ce79f9c50570e3f77e9
Kaiden Buchter <150194619+Kaiden-Buchter@users.noreply.github.com>
2023-12-07 19:33:29 +0000

    Add new commands to REPL

--- main.ts
+++ main.ts
@@ -31,13 +31,47 @@ function repl() {
       console.log("\n   Help Menu   \n");
       console.log("exit - Exit Repl");
       console.log("help - Help Menu");
+      console.log("about - About Z- Programming Language");
       console.log("clear - Clear Repl");
-      
+      console.log("env - Print Environment");
+      console.log("write - Write to file");
+      console.log("anything else - Evaluate Expression");
+      console.log("\n");
+      console.log("Note: The repl is not perfect and may crash if you type something wrong.");
+      console.log("If this happens, just restart the repl.");
+      console.log("\n")
       continue;
     }
 
     if (!input || input.includes("clear")) {
       console.clear();
+      continue;
+    }
+
+    if (!input || input.includes("env")) {
+      console.log("\n   Environment   \n");
+      console.log(env);
+      continue;
+    }
+
+    if (!input || input.includes("about")) {
+      console.log("\n   About   \n");
+      console.log("Z- is a programming language created by Kaiden Buchter.");
+      console.log("It is a work in progress and is not yet complete.");
+      console.log("The source code is available at https://github.com/Kaiden-Buchter/Z-");
+      continue;
+    }
+
+    if (!input || input.includes("write")) {
+      console.log("\n   Write   \n");
+      const fileName = prompt("File Name: ");
+      const file = Deno.openSync(fileName!, { write: true, create: true });
+      const encoder = new TextEncoder();
+      const input = prompt("Input: ");
+      const text = input
+      const data = encoder.encode(text!);
+      Deno.writeSync(file.rid, data);
+      Deno.close(file.rid);
       continue;
     }
 
